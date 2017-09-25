634325bc3100abb2cda6ff376e4d3efc87e8c86d
callumjones17 <s3601120@student.rmit.edu.au>
2017-09-25 12:37:47 +1000

    Added - Arduino Code
    The arduino code is used to debug/test to see if the serial is working.

---
+++ Talk_to_OUSB/RX/RX.ino
@@ -0,0 +1,20 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial OUSB(7,8); // RX, TX
+
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(13,OUTPUT);
+ digitalWrite(13,HIGH);
+Serial.begin(9600);
+OUSB.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ if (OUSB.available()>0){
+  int r = OUSB.read();
+  Serial.write(r);
+ }
+ delay(30);
+}
---
+++ Talk_to_OUSB/TX/TX.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial OUSB(2,3); // RX, TX
+
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(13,OUTPUT);
+ digitalWrite(13,HIGH);
+Serial.begin(9600);
+OUSB.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ if (Serial.available()>0){
+  char r = Serial.read();
+  OUSB.write(r);
+  Serial.write(r);
+ }
+ delay(20);
+}
