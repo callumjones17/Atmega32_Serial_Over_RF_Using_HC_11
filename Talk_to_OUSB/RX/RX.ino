85a466567e3039de333df9ea1629607a6c57043e
callumjones17 <s3601120@student.rmit.edu.au>
2017-10-10 15:23:48 +1100

    Added Comments

--- Talk_to_OUSB/RX/RX.ino
+++ Talk_to_OUSB/RX/RX.ino
@@ -4,17 +4,17 @@ SoftwareSerial OUSB(7,8); // RX, TX
 
 void setup() {
   // put your setup code here, to run once:
- pinMode(13,OUTPUT);
- digitalWrite(13,HIGH);
-Serial.begin(9600);
-OUSB.begin(9600);
+ pinMode(13,OUTPUT);       \\ Set up pin 13 as output
+ digitalWrite(13,HIGH);   \\ Make 13 a high (has an led connected).
+Serial.begin(9600);       \\ Open Serial Port with PC.
+OUSB.begin(9600);         \\ Open Serial Port with OUSB
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- if (OUSB.available()>0){
-  int r = OUSB.read();
-  Serial.write(r);
+ if (OUSB.available()>0){     \\ If OUSB sends data through
+  int r = OUSB.read();        \\ Read it 
+  Serial.write(r);            \\ And send it to the PC.
  }
- delay(30);
+ delay(30);                   \\ Delay
 }
